ea79c9b0fa6db8be4959e393524b90d4b7bf6b1c
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2024-07-23 15:29:09 -0400

    Primeiro exemplo

---
+++ olamundo/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Primeiro exemplo PHP</title>
+        <style>
+            h2{
+                color: #000000;
+                text-shadow: 1px 1px 1px black;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+            echo "<h2>Olá,<br> Mundo!</h2>";
+        ?>
+    </body>
+</html>
