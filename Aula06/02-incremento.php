a1f35df261b651f97ccba08d982a594ab35b9ebe
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2024-07-25 15:55:58 -0400

    Aula 06

---
+++ Aula06/02-incremento.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Incremento</title>
+        <<link rel="stylesheet" href="_css/estilo.css"/>
+    </head>
+    <body>
+        <div>
+            <?php
+                /*
+                    Esse exercício pretende demonstrar o uso
+                    de operadores de incremento e decremento
+                */
+                $atual = $_GET["aa"]; // Essa linha vai pegar o anp na URL
+                echo "O ano atual é $atual e o ano anterior é " . --$atual;
+            ?>
+        </div>
+    </body>
+</html>
