6ac2d4108fd38b7a78929e32368598583f6098a2
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2024-08-01 22:14:59 -0400

    aula 11

---
+++ aula11/01-contador.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="_css/estilo.css"/>
+    </head>
+    <body>
+        <div>
+            <?php
+                $c = 1;
+                while ($c <= 10){
+                    echo "$c<br>";
+                    $c ++;
+                }
+            ?>
+        </div>
+    </body>
+</html>
---
+++ aula11/02-parte2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="_css/estilo.css"/>
+    </head>
+    <body>
+        <div>
+            <?php
+                
+            ?>
+        </div>
+    </body>
+</html>
