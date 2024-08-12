49b0422d5ce7971a005944c0954a231f06484196
Henrique Santos <154251044+Henrique-Santoss@users.noreply.github.com>
2024-08-12 15:14:03 -0400

    Aula 15

---
+++ aula15/02-funcao.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="_css/estilo.css"/>
+    </head>
+    <body>
+        <div>
+            <?php
+                include "funcoes.php";
+                echo "<h1>Testando novas funcções</h1>";
+                ola();
+                mostraValor(4);
+                echo "<h2>Finalizando o Programa...</h2>";
+            ?>
+        </div>
+    </body>
+</html>
